c3847dec0700ae39c6da962a829bf0f03c82f11b
Frank Joppe <frankjoppe@hotmail.com>
2020-01-19 20:26:22 +0100

    Fixing infinite gosubs issue

--- tests/Test.Legivel.ThompsonParser/FF - Thompson Construct.fsx
+++ tests/Test.Legivel.ThompsonParser/FF - Thompson Construct.fsx
@@ -13,95 +13,46 @@ open NLog
 
 #load "nlog.fsx"
 
-let ``s-indent(n)`` = Repeat(RGP (HardValues.``s-space``, [Token.``t-space``]), 0)
-let ``s-indent(<n)`` = Range(RGP (HardValues.``s-space``, [Token.``t-space``]), 0, -1) (* Where m < n *)
-
-let ``s-flow-line-prefix`` = (``s-indent(n)``) + OPT(HardValues.``s-separate-in-line``)
-
-let ``s-line-prefix Flow-in`` = ``s-flow-line-prefix``
-
-let ``l-empty Flow-in`` = ((``s-line-prefix Flow-in``) ||| (``s-indent(<n)``)) + HardValues.``b-as-line-feed``
-
-let ``b-l-trimmed Flow-in`` = HardValues.``b-non-content`` + OOM(``l-empty Flow-in``)
-
-
-let ``b-l-folded Flow-in`` = ``b-l-trimmed Flow-in`` ||| HardValues.``b-as-space``
-
-let ``s-flow-folded`` =
-    OPT(HardValues.``s-separate-in-line``) + (``b-l-folded Flow-in``) + ``s-line-prefix Flow-in``
-
-
-let ``s-double-escaped`` = ZOM(HardValues.``s-white``) + HardValues.``c-escape`` + HardValues.``b-non-content`` + ZOM(``l-empty Flow-in``) + (``s-flow-line-prefix``)
-
-let ``s-double-break`` = (``s-double-escaped``) ||| (``s-flow-folded``)
-
-let ``s-double-next-line`` =  
-    ZOM((``s-double-break``) + HardValues.``ns-double-char`` + HardValues.``nb-ns-double-in-line``) + (``s-double-break``) |||
-    OOM((``s-double-break``) + HardValues.``ns-double-char`` + HardValues.``nb-ns-double-in-line``) + ZOM(HardValues.``s-white``)
-
-
-let ``nb-double-multi-line`` = HardValues.``nb-ns-double-in-line`` + ((``s-double-next-line``) ||| ZOM(HardValues.``s-white``))
-
-let ``nb-double-text`` = ``nb-double-multi-line`` 
-
-let ``c-double-quoted`` = HardValues.``c-double-quote`` + GRP(``nb-double-text``) + HardValues.``c-double-quote``
 
 
 NlogInit.With __SOURCE_DIRECTORY__ __SOURCE_FILE__
 
 let logger = LogManager.GetLogger("*")
 
+let ``s-indent(n)`` = Repeat(RGP (HardValues.``s-space``, [Token.``t-space``]), 0)
 
+let ``s-flow-line-prefix`` = (``s-indent(n)``) + OPT(HardValues.``s-separate-in-line``)
+let ``s-separate-lines`` = (HardValues.``s-l-comments`` + (``s-flow-line-prefix``)) ||| HardValues.``s-separate-in-line``
+let ``s-separate`` = ``s-separate-lines``
 
-//c-double-quoted	 loc:(2,1) i:0 c:Flow-out &a:0 e:3 w:0 sp:1
-//s-line-prefix	 loc:(2,1) i:0 c:Flow-in &a:0 e:3 w:0 sp:1
-//s-line-prefix	 loc:(2,1) i:0 c:Flow-in &a:0 e:3 w:0 sp:1
-//s-line-prefix	 loc:(2,1) i:0 c:Flow-in &a:0 e:3 w:0 sp:1
-//s-line-prefix	 loc:(2,1) i:0 c:Flow-in &a:0 e:3 w:0 sp:1
-//s-line-prefix	 loc:(2,1) i:0 c:Flow-in &a:0 e:3 w:0 sp:1
-//s-line-prefix	 loc:(2,1) i:0 c:Flow-in &a:0 e:3 w:0 sp:1
+let nfa = ``s-separate`` |> rgxToNFA
 
-
-
-let nfa = ``c-double-quoted`` |> rgxToNFA
 PrintIt nfa
 
-//HardValues.``ns-double-char``
-//|>  rgxToNFA
-//|>  PrintIt
 
-//(RGP ("\\\\", [Token.``c-escape``]) + RGP("A", [Token.``nb-json``])) |||
-//(((HardValues.``nb-json`` - RGO("\\\\\"", [Token.``c-escape``; Token.``t-double-quote``]) - HardValues.``s-white``)) + RGP("B", [Token.``nb-json``]))
-//|>  rgxToNFA
-//|>  PrintIt
-
-
-
-
-
-//(RGP("\n", [Token.NewLine]) + RGP("A", [Token.``nb-json``])) ||| 
-//(RGO("\t\n", [Token.``t-tab``;Token.NewLine]) + RGP("X", [Token.``nb-json``])) |||
-//(RGP("\t", [Token.``t-tab``]) + RGP("Y", [Token.``nb-json``]))
-//|>  rgxToNFA
-//|>  PrintIt
-
-
+//s-l+block-scalar	 loc:(2,5) i:0 c:Block-out &a:0 e:0 w:0 sp:5
+//s-separate	 loc:(2,5) i:1 c:Block-out &a:0 e:0 w:0 sp:5
+//c-ns-properties	 loc:(4,3) i:0 c:Block-out &a:0 e:0 w:0 sp:37
+//s-separate	 loc:(4,3) i:0 c:Block-out &a:0 e:0 w:0 sp:37
+//s-separate	 loc:(4,3) i:0 c:Block-out &a:0 e:0 w:0 sp:37
+//c-l+literal	 loc:(4,3) i:0 c:Block-out &a:0 e:0 w:0 sp:37
 
 let yaml = "
-\"Fun with \\\\
-\\\" \\a \\b \\e \\f \\
-\\n \\r \\t \\v \\0 \\
-\\  \\_ \\N \\L \\P \\
-\\x41 \\u0041 \\U00000041\"
+key:    # Comment
+        # lines
+  value
+
 "
 
 let stream = RollingStream<_>.Create (tokenProcessor yaml) (TokenData.Create (Token.EOF) "")
-stream.Position <- 1
+stream.Position <- 5
 
 
 let r = parseIt nfa stream
 
-r.Groups.[0] |> clts
+r.FullMatch
+
+
 
 
 
